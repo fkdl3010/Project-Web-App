f6b3582004f590dae3ebd989d6185642703e24ee
fkdl3010 <fkdl3919@icloud.com>
2021-01-11 06:47:25 +0900

    update 2021-01-11 post

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
+/* eslint-disable */
 import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
 
-  let [a,b] = useState('남자 코트 추천');
+  let [글제목, 글제목변경] = useState(['남자 코트 추천','강남 우동맛집','자바 독학'] );
+  let [따봉, 따봉변경] = useState(0)
 
-  let posts = '강남 고기 맛집';
+  function 제목바꾸기(){
+    var newArray = [...글제목];
+    newArray[0] = '여자코트 추천';
+    글제목변경(newArray);
+  }
   return (
     
     <div className="App">
       <div className="black-nav">
         <div>개발 Blog</div>
       </div>
+      <button onClick = { 제목바꾸기 }>버튼</button>
       <div className="list">
-        <h3>{posts}</h3>
+        <h3>{글제목[0]} <span onClick={ ()=>{ 따봉변경(따봉 + 1)}}>👍</span> {따봉} </h3>
+        <p>1월 17일 발행</p>
+        <hr/>
+      </div>
+      <div className="list">
+        <h3>{글제목[1]}</h3>
         <p>2월 17일 발행</p>
         <hr/>
-
+      </div>
+      <div className="list">
+        <h3>{글제목[2]}</h3>
+        <p>3월 17일 발행</p>
+        <hr/>
       </div>
     </div>
   );
